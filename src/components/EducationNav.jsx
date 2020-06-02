27dd8cf8fef7870872632410a6b01cead9af91ce
Ivan5 <converse_ivan@hotmail.com>
2020-06-02 15:59:06 -0500

    Making the styles in the pages about education

---
+++ src/components/EducationNav.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const EducationNav = () => {
+  return (
+    <div>
+      <h1>Nav</h1>
+    </div>
+  )
+}
+
+export default EducationNav
--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -2,10 +2,48 @@ import React from "react"
 import { graphql } from "gatsby"
 
 const Template = props => {
+  const pageData = props.data.educationJson
+
   return (
-    <header>
-      <h2>Template</h2>
-    </header>
+    <>
+      <header className="py-12 border-purple-500 border-solid border-t-8">
+        <div className="max-w-4xl mx-auto">
+          <h2 className="capitalize text-6xl font-bold">{pageData.title}</h2>
+          <p className="page-xl">{pageData.description}</p>
+        </div>
+      </header>
+      <ul>
+        {pageData.items.map((item, index) => (
+          <li key={index} className="bg-white shadow mt-4 flex">
+            <p className="vertical-text">{pageData.slug}</p>
+            <div className="flex items-center flex-1 p-8">
+              <div className="flex-1">
+                <h3>{item.name}</h3>
+                {item.degree && (
+                  <span className="inline-block p-2 radius bg-purple-100 text-purple-700">
+                    {item.degree}
+                  </span>
+                )}
+                {item.url && (
+                  <a
+                    href={item.url}
+                    className="btn mt-4 inline-block"
+                    target="_blank"
+                  >
+                    Ver más
+                  </a>
+                )}
+              </div>
+              <div className="inline-block">
+                <span className="inline-block p-2 text-2xl bg-green-100 text-green-700">
+                  {item.score}
+                </span>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
@@ -16,6 +54,7 @@ export const query = graphql`
     educationJson(slug: { eq: $slug }) {
       title
       description
+      slug
       items {
         name
         degree
